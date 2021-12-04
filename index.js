4b4cc8c9396a2d136fe7ccf44f696d3b92d36f0a
Anthony Gedeon <anthony.gedeonn@gmail.com>
2021-12-03 23:50:09 -0600

    refact: improve grid interface

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
+"use strict";
+var scale = 20;
+var WIDTH = 640;
+var HEIGHT = 320;
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 640;
-canvas.height = 320;
-ctx.fillStyle = "green";
-ctx.fillRect(0, 0, 10, 10);
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+ctx.strokeStyle = "#cacaca";
+ctx.globalCompositeOperation = "destination-over";
+var grid = [];
+var createGrid = function () {
+    for (var i = 0; i < HEIGHT / scale; i++) {
+        grid[i] = [];
+        for (var j = 0; j < WIDTH / scale; j++) {
+            ctx.strokeRect(j * scale, i * scale, scale, scale);
+            grid[i][j] = { x: j, y: i, isAlive: false };
+        }
+    }
+};
+createGrid();
+canvas.addEventListener("click", function (e) {
+    var rect = canvas.getBoundingClientRect();
+    var x = Math.floor((e.clientX - rect.left) / scale);
+    var y = Math.floor((e.clientY - rect.top) / scale);
+    grid[y][x] = { x: x, y: y, isAlive: true };
+    grid.forEach(function (row) {
+        row.forEach(function (col) {
+            if (col.isAlive) {
+                console.log(grid);
+                ctx.fillRect(col.x * scale, col.y * scale, scale, scale);
+            }
+        });
+    });
+});
+window.addEventListener("load", function () {
+    createGrid();
+});
+document.addEventListener("wheel", function (e) {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    scale += e.deltaY * -0.02;
+    scale = Math.min(Math.max(8, scale), 20);
+    createGrid();
+});
--- index.ts
+++ index.ts
@@ -1,37 +1,58 @@
+let scale = 20;
+interface Cell {
+    x: number;
+    y: number;
+    isAlive: boolean;
+}
+
+const WIDTH = 640;
+const HEIGHT = 320;
+
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-const cell = {
-    width: 20,
-    height: 20,
-};
-
-let scale = 20;
-
-canvas.width = 640;
-canvas.height = 320;
-
-canvas.addEventListener("click", (e) => {
-    const rect = canvas.getBoundingClientRect();
-    const x = Math.floor((e.clientX - rect.left) / scale) * scale;
-    const y = Math.floor((e.clientY - rect.top) / scale) * scale;
-    ctx.fillRect(x, y, scale, scale);
-    console.log(x, y);
-});
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
 
 ctx.strokeStyle = "#cacaca";
 ctx.globalCompositeOperation = "destination-over";
 
-document.addEventListener("wheel", (e) => { 
-    ctx.clearRect(0, 0, 640, 320);
+const grid: Cell[][] = []
 
-    for (let i = 0; i < canvas.width; i += scale) {
-        for (let j = 0; j < canvas.height; j += scale) {
-           ctx.strokeRect(i, j, scale, scale);
+const createGrid = () => {
+    for (let i = 0; i < HEIGHT / scale; i++) {
+        grid[i] = [];
+        for (let j = 0; j < WIDTH / scale; j++) {
+            ctx.strokeRect(j*scale, i*scale, scale, scale);
+            grid[i][j] = {x: j, y: i, isAlive: false};
         }
     }
+}
 
-   scale += Math.floor((e.deltaY * -0.01) - 0.03);
+createGrid();
 
-   console.log((e.deltaY * -0.01) - 0.03);
+canvas.addEventListener("click", (e) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = Math.floor((e.clientX - rect.left) / scale);
+    const y = Math.floor((e.clientY - rect.top) / scale);
+    grid[y][x] = {x, y, isAlive: true};
+    grid.forEach(row => {
+       row.forEach(col => {
+           if (col.isAlive) {
+               console.log(grid);
+               ctx.fillRect(col.x*scale, col.y*scale, scale, scale);
+           }
+       })
+   }) 
+});
+
+window.addEventListener("load", () => {
+    createGrid();
+});
+
+document.addEventListener("wheel", (e) => { 
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    scale += e.deltaY * -0.02;
+    scale = Math.min(Math.max(8, scale), 20);
+    createGrid();
 });
